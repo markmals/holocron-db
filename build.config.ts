372d60137a04ae52a47845817934034fc3bc13c5
Mark Malström <mark@malstrom.me>
2022-11-27 21:41:25 -0600

    Actually fixed import paths

--- build.config.ts
+++ build.config.ts
@@ -1,15 +1,24 @@
-import { defineBuildConfig } from "unbuild"
+import { defineBuildConfig, MkdistBuildEntry } from "unbuild"
+
+let src: MkdistBuildEntry = { builder: "mkdist", input: "./src", outDir: "./dist" }
+let firestore: MkdistBuildEntry = {
+    builder: "mkdist",
+    input: "./src/firestore",
+    outDir: "./dist/firestore",
+}
+
+let esm: MkdistBuildEntry[] = [src, firestore].map(entry => ({
+    ...entry,
+    format: "esm",
+}))
+
+let cjs: MkdistBuildEntry[] = [src, firestore].map(entry => ({
+    ...entry,
+    format: "cjs",
+    ext: "cjs",
+}))
 
 export default defineBuildConfig({
-    entries: [
-        "./src/index",
-        // mkdist builder transpiles file-to-file keeping original sources structure
-        {
-            builder: "mkdist",
-            input: "./src/firestore",
-            outDir: "./dist/firestore",
-        },
-    ],
-
+    entries: [...cjs, ...esm],
     declaration: true,
 })
